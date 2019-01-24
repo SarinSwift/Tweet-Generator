590c7ac9e829ec9b2d23c42a14be1d193f9fabf3
SarinSwift <sarinyaswift@gmail.com>
2019-01-23 17:49:21 -0800

    added python_quotes module

--- python_quote.py
+++ python_quote.py
@@ -5,5 +5,10 @@ quotes = ("It's just a flesh wound.",
           "He's not the Messiah. He's a very naughty boy!",
           "THIS IS AN EX-PARROT!!")
 
-rand_index = random.randint(0, len(quotes) - 1)
-print(quotes[rand_index])
+def random_python_quote():
+    rand_index = random.randint(0, len(quotes) - 1)
+    return quotes[rand_index]
+
+if __name__ == '__main__':
+    quote = random_python_quote()
+    print(quote)

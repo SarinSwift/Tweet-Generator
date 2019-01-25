8808d8f2a41102bfcfd5fdf651e49f67aae3a8c4
SarinSwift <sarinyaswift@gmail.com>
2019-01-24 19:28:17 -0800

    string reversals with words and sentences

--- reverse_words.py
+++ reverse_words.py
@@ -1,4 +1,4 @@
-# reverse strings 
+# reverse strings
 
 def reverse_word(word):
     # return "".join(reversed(word))
@@ -7,6 +7,18 @@ def reverse_word(word):
     return ''.join(word[i] for i in range(len(word)-1, -1, -1))
 
 
+def reverse_sentence(sentence):
+    splited = sentence.split()
+    reversedArray = []
+
+    for i in range(len(splited)-1, -1, -1):
+        reversedArray.append(splited[i])
+
+    return ' '.join(reversedArray)
+
 if __name__ == '__main__':
     word = "look"
     print(reverse_word(word))
+
+    sentence = "sarin is coding"
+    print(reverse_sentence(sentence))

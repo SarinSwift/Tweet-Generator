245d748d010616efb9c54875d313add4bcacf993
SarinSwift <sarinyaswift@gmail.com>
2019-01-28 17:02:29 -0800

    fixed bug in rearranging function

--- rearrange.py
+++ rearrange.py
@@ -6,19 +6,23 @@ import random
 def rearranging():
     # ['how', 'now', 'brown', 'cow']
     arrayOfWords = sys.argv[1:]
-    answerString = " "
 
     # for i in the len of (array - 1) down until 0 by going down -1 each time
     for i in range(len(arrayOfWords)-1, 0, -1):
         # picking a rondom index from 0...i
-        rand_index = random.randint(0, i+1)
+        rand_index = random.randint(0, i)
         # basically swaping the current index item with the random index item in the array
+        arrayOfWords[i] = arrayOfWords[i].lower()
         arrayOfWords[i], arrayOfWords[rand_index] = arrayOfWords[rand_index], arrayOfWords[i]
 
-    return answerString.join(arrayOfWords)
+
+    answer = " ".join(arrayOfWords)
+    answer = answer.capitalize() + "."
+
+    return ("\033[1;32;40m {0} \n").format(answer)
 
 
- # 2nd method using choice() method 
+ # 2nd method using choice() method
 def rearrange():
     array = sys.argv[1:]
     randomized = []
@@ -35,4 +39,4 @@ def rearrange():
 
 if __name__ == '__main__':
     print(rearranging())
-    print(rearrange())
+    # print(rearrange())

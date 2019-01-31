37577e097749d35a80fff2271f95ad93adce3a17
SarinSwift <sarinyaswift@gmail.com>
2019-01-30 17:28:57 -0800

    added comments in random sentence generator

--- dictonary_words.py
+++ dictonary_words.py
@@ -1,6 +1,7 @@
 import random
 import sys
 
+# 2. Random Dictionary Words
 
 def get_random_word(file):
     sentence_string = ""
@@ -9,9 +10,10 @@ def get_random_word(file):
     with open(file, 'r') as f:
         content_of_file = f.read()
 
+
+    lines = content_of_file.split()
     for i in range(0, int(array_of_number[0])):
-        lines = content_of_file.split()
-        line_number = random.randrange(0, len(lines))
+        line_number = random.randrange(len(lines))
 
         sentence_string += (lines[line_number] + " ")
 
@@ -22,18 +24,25 @@ def random_sentence_from_dict(file):
     array_string = []
     array_of_number = sys.argv[1]
 
+    # make an indented block that acesses the file and as soon as you leave the block, it automatically closes the file
     with open(file, 'r') as f:
+        # gets everything from the file (one huge entire string of content)
         content_of_file = f.read()
 
+    # an array of words in the file we read from
+    lines = content_of_file.split()
     for i in range(0, int(array_of_number[0])):
-        lines = content_of_file.split()
-        line_number = random.randrange(0, len(lines))
+
+        # random number in the range 0 -> len of lines
+        # gets valid indexes
+        line_number = random.randrange(len(lines))
 
         array_string.append(lines[line_number])
 
     answer_string = " ".join(array_string) + "."
 
     return answer_string.capitalize()
+
 
 
 if __name__ == '__main__':

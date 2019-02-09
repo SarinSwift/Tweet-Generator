c51280d24087397f075a2ee652e39bb0a73b7f9e
SarinSwift <sarinyaswift@gmail.com>
2019-02-09 12:14:27 -0800

    set up virtual env and develop web app locally

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+import analyze_word_freq as ana
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    findingHist = ana.histogram()
+    return str(findingHist)

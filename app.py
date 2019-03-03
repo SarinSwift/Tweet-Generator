d39122738541d42f3af3ffc10c608c2b8cd56de0
SarinSwift <sarinyaswift@gmail.com>
2019-03-03 14:04:56 -0800

    updated app.py to generate sentences from markov chain

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 import analyze_word_freq as ana
 
+import markov_chain
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     # findingHist = ana.histogram()
-    return "hello world"
+    longer_model = markov_chain.markov_chain(markov_chain.open_file('/Users/sarinswift/Documents/CS1.2/WarAndPeace.txt'))
+    return markov_chain.generate_sentence(10, longer_model)
+    # return "Hellooo"

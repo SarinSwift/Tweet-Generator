b6c97d5f5a74115cf4fbf79da78b70bf8a5b16e0
SarinSwift <sarinyaswift@gmail.com>
2019-03-03 14:27:17 -0800

    updated the path to textfile

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     # findingHist = ana.histogram()
-    longer_model = markov_chain.markov_chain(markov_chain.open_file('/Users/sarinswift/Documents/CS1.2/WarAndPeace.txt'))
+    longer_model = markov_chain.markov_chain(markov_chain.open_file('WarAndPeace.txt'))
     return markov_chain.generate_sentence(10, longer_model)
     # return "Hellooo"
